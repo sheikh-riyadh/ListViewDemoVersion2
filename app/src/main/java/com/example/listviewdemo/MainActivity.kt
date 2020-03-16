dd8e73b1c341ba926ab3cb07b7001c90476ac8d4
Sheikh Riyadh <sheikhriyadh350883@gmail.com>
2020-03-16 17:04:55 +0600

    We added some component like dataBinding and more some..

--- app/src/main/java/com/example/listviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/listviewdemo/MainActivity.kt
@@ -2,21 +2,29 @@ package com.example.listviewdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.databinding.DataBindingUtil
+import com.example.listviewdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var listView:ListView
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var listView: ListView
     lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var country_names: Array<out String> = resources.getStringArray(R.array.country_names)
 
-        listView = findViewById(R.id.listViewID)
+        // in this line we did hide title in the full screen..
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        var country_names: Array<out String> = resources.getStringArray(R.array.country_names)
         adapter = ArrayAdapter(this,R.layout.sample_view,R.id.txtID,country_names)
-        listView.adapter = adapter
+       binding.listViewID.adapter = adapter
 
 
 
